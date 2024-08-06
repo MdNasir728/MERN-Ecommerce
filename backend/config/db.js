7211e6aa813f20d33c88c647c71770e484f852bb
MD NASIR <ipsmdnasir786@gmail.com>
2024-08-06 23:23:59 +0530

    first commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,18 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose")
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log(`Successfully connnected to mongoDB 👍`);
-  } catch (error) {
-    console.error(`ERROR: ${error.message}`);
-    process.exit(1);
-  }
-};
 
-export default connectDB;
+async function connectDB(){
+    try{
+        const res = await mongoose.connect(process.env.MONGODB_URI)
+        if (res) {
+            console.log("connnected to DB")
+
+        }
+    }catch(err){
+        console.log(err)
+        console.log("Connnection to DB failed")
+
+    }
+}
+
+module.exports = connectDB
